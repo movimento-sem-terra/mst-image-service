da6f21f59942feaf9e504a901d51e5fcd75aca7f
Rafael Garcia <rafbgarcia@gmail.com>
2014-06-02 13:09:25 -0300

    Returning error on upload exception.

--- app.rb
+++ app.rb
@@ -21,23 +21,26 @@ get "/upload" do
   haml :upload
 end      
 
-post "/upload" do 
-  
-  token = params['token'] || ''
-  user  = User.new(token)
+post "/upload" do   
+  begin
+    token = params['token'] || ''
+    user  = User.new(token)
 
-  return "U don't have access bro." unless user.authorized?
+    return "U don't have access bro." unless user.authorized?
 
-  path = Dir.mktmpdir('upload')
-  name_file = params['myfile'][:filename]
-  file_path = "#{path}/#{name_file}" 
+    path = Dir.mktmpdir('upload')
+    name_file = params['myfile'][:filename]
+    file_path = "#{path}/#{name_file}" 
 
-  File.open(file_path, "w") do |f|
-    f.write(params['myfile'][:tempfile].read)
+    File.open(file_path, "w") do |f|
+      f.write(params['myfile'][:tempfile].read)
+    end
+
+    service = Service::Flickr.new 
+
+    return service.upload file_path
+  rescue Exception => e
+    return e.message
   end
-
-  service = Service::Flickr.new 
-
-  return service.upload file_path
 end
 
