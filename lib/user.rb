6dc263985bc5d0982d87a41554565d7d43d41cc0
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-06-04 02:25:51 -0300

    more small formatting

--- lib/user.rb
+++ lib/user.rb
@@ -9,8 +9,8 @@ class User
 
   def authorized?
     response = HTTParty.get(ORGS_ENDPOINT, headers: {
-      "Authorization" => "token #{@token}",
-      "User-Agent" => 'Application'
+      'Authorization' => "token #{@token}",
+      'User-Agent' => 'Application'
     })
     Array(response).flatten.find do |org|
       org['id'].to_s == ENV['REPO_ID'] 
