35014acc8a7aebe7197e103961edb6dca70062c2
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-06-04 02:21:08 -0300

    removes duplication on repo_id assignment

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,6 +4,7 @@ require_relative '../../lib/user.rb'
 class UserTest < ActiveSupport::TestCase
 
   setup  do
+    ENV['REPO_ID'] = '23'
     @token = 'lalalala'
     @user = User.new @token
   end
@@ -11,36 +12,26 @@ class UserTest < ActiveSupport::TestCase
   test 'is authorized when having the repo id among his orgs' do
     response = [{'id' => 12},{'id' => 20},{'id' => 23}]
     HTTParty.stubs(:get).returns(response)
-    
-    ENV['REPO_ID'] = '23'
-    
     assert @user.authorized?
   end
 
   test 'is not authorized when not having the repo id among his orgs' do
-    response = [{'id' => 12},{'id' => 20},{'id' => 23}]
+    response = [{'id' => 12},{'id' => 20},{'id' => 45}]
     HTTParty.stubs(:get).returns(response)
-    
-    ENV['REPO_ID'] = '45'
-    
     assert !@user.authorized?
   end
 
   test 'is not authorized when the response is nil' do
-    ENV['REPO_ID'] = '45'
     HTTParty.stubs(:get).returns(nil)
     assert !@user.authorized?
   end
 
   test 'is not authorized when the response is empty' do
-    ENV['REPO_ID'] = '45'
     HTTParty.stubs(:get).returns('')
     assert !@user.authorized?
   end
 
   test 'is not authorized when providing bad credentials' do
-    ENV['REPO_ID'] = '45'
-
     HTTParty.stubs(:get).returns([{message:'Bad Credential'}])
     assert !@user.authorized?
 
