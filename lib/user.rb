8cb4a9c01d49ed28b41b13d29d41a495d5c521b9
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-06-04 01:17:40 -0300

    extracts constant for orgs endpoint

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,17 @@
 require 'httparty'
 
 class User
+  ORGS_ENDPOINT = 'https://api.github.com/user/orgs'
 
   def initialize token
     @token = token
   end
 
   def authorized?
-    response = HTTParty.get('https://api.github.com/user/orgs', headers: {"Authorization" => "token #{@token}",
-                                                                          "User-Agent" => 'Application' })
-
+    response = HTTParty.get(ORGS_ENDPOINT, headers: {
+      "Authorization" => "token #{@token}",
+      "User-Agent" => 'Application'
+    })
     unless response.to_s.empty?
       values = Array(response).flatten
       (not values.index { |org| org['id'].to_s == ENV['REPO_ID'] }.nil?)
