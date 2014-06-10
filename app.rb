d2da6fa65df00a22e1f4cae18f835ab593f8460a
jpaulorio <jpaulorio@outlook.com>
2014-06-10 17:08:27 -0300

    Adds content-type to the allowed headers

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require_relative 'lib/user.rb'
 before do
   headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
   headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
+  headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin, content-type'
   headers['Access-Control-Allow-Credentials'] = 'true'
 end
 
