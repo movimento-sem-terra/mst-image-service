5aeb384ce4004a701d2ee635f1673411c136217b
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-07-07 14:08:06 -0300

    #80 add google email adress as enviroment variable

--- lib/google_drive.rb
+++ lib/google_drive.rb
@@ -55,14 +55,14 @@ module Service
     private
 
     def api_service
-      key = Google::APIClient::KeyUtils.load_from_pkcs12('a3ac5bd54d15c78a70383c26d1e4f0c54a2bb728-privatekey.p12', 'notasecret')
+      key = Google::APIClient::KeyUtils.load_from_pkcs12('a0fb8bd517b739e626aff1ed6a0e945d92080cd9-privatekey.p12', 'notasecret')
       client = Google::APIClient.new
       client.authorization = Signet::OAuth2::Client.new(
-        :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-        :audience => 'https://accounts.google.com/o/oauth2/token',
-        :scope => 'https://www.googleapis.com/auth/drive',
-        :issuer => '205788113661-sgp648hint92qnrofku2ebl45q10ldkf@developer.gserviceaccount.com',
-        :signing_key => key)
+        token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+        audience: 'https://accounts.google.com/o/oauth2/token',
+        scope: 'https://www.googleapis.com/auth/drive',
+        issuer: ENV['GOOGLE_EMAIL_ADDRESS'],
+        signing_key: key)
 
       client.authorization.fetch_access_token!
 
