4d09fad0703bcc48943b6d749881dc237baeb9d5
RatoX <rodrigovieira18+raphael+rodrigo@gmail.com>
2014-11-04 18:31:51 -0300

    authorized removido

--- lib/user.rb
+++ lib/user.rb
@@ -7,16 +7,16 @@ class User
     @token = token
   end
 
-  def authorized?
-    repos = ENV['REPO_ID'].split ','
-    response = HTTParty.get(ORGS_ENDPOINT, headers: {
-      'Authorization' => "token #{@token}",
-      'User-Agent' => 'Application'
-    })
-    Array(response).flatten.find do |org|
-      repos.include? org['id'].to_s
-    end
-  end
+  # def authorized?
+  #   repos = ENV['REPO_ID'].split ','
+  #   response = HTTParty.get(ORGS_ENDPOINT, headers: {
+  #     'Authorization' => "token #{@token}",
+  #     'User-Agent' => 'Application'
+  #   })
+  #   Array(response).flatten.find do |org|
+  #     repos.include? org['id'].to_s
+  #   end
+  # end
 
   def enviromment_config
     JSON.parse ENV['CONFIG']
